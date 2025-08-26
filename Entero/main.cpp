d35e6d3abca50e6e84a1e97f30e1ed925c07cde9
Derek <derek.loayza@ucsp.edu.pe>
2025-08-25 22:06:35 -0500

    If y else

--- Entero/main.cpp
+++ Entero/main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    int val =1;
-    cout << "Hello world!" << endl;
+    int numero1=0, numero2=0;
+    cout << "Dame dos numeros" << endl;
+    cin >> numero1 >> numero2;
+    if (numero1 > numero2){
+        cout <<"El primero numero es el mayor";
+    }
+    else
+        cout <<"El segundo numero es el mayor";
+
     return 0;
 }
