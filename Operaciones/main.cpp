0c1b649ad6c22646c31bb647ab49497fb308bd4b
DerekLoayza <derek.loayza@ucsp.edu.pe>
2025-08-29 08:23:32 -0500

    Operaciones

---
+++ Operaciones/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    suma +
+    resta -
+    multi *
+    divi /
+    modulo %
+    */
+    int val=100;
+    int result1=val % 2;
+    int result2=val / 10;
+    cout << "El resultado es: " << result1 << endl;
+    cout << "El resultado 2 es: " << result2 << endl;
+//Implementa un programa que reciba por teclado un numero de 4 digitos e implementa el numero en forma vertical
+
+    return 0;
+}
