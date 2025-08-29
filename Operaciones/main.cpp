1514ec92a3444fa50ca70224d188a9ebb086c25e
DerekLoayza <derek.loayza@ucsp.edu.pe>
2025-08-29 08:25:37 -0500

    Update main.cpp

--- Operaciones/main.cpp
+++ Operaciones/main.cpp
@@ -11,12 +11,13 @@ int main()
     divi /
     modulo %
     */
-    int val=100;
-    int result1=val % 2;
+    int a=102;
+    int result1=val % 10;
     int result2=val / 10;
     cout << "El resultado es: " << result1 << endl;
     cout << "El resultado 2 es: " << result2 << endl;
 //Implementa un programa que reciba por teclado un numero de 4 digitos e implementa el numero en forma vertical
-
+    HACERLO CON DIVISIONES, DIVIDIR ENTRE 1000, HACER EL MODULO, NUM /100, NUM+100*2 ASI ASI ASI
     return 0;
 }
+
