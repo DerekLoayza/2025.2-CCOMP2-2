8551851775789b1ce333c2b4d99ba94304986671
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-09-05 08:24:25 -0500

    Primos y palindromos

--- Bucles/main.cpp
+++ Bucles/main.cpp
@@ -7,48 +7,80 @@ int main()
             instruccion 1
             instruccion 2
 
-            incrementacion
-        } */
-    int i=1;
-    while (i<10){//Si quieres que tambien imprima el 10 pones <=
-        cout << i << endl;
-        i=i+1; }
-    i=9;
-    while (i>=1){
-        cout << i << endl;
-        i=i-1;
+            incrementacion }
+    break: Rompe el bucle
+    continue: Salta a la siguiente iteracion. Si llega a un if, que continue y no se detenga
+         */
+
+//Programa que indique los numeros de 1 al 9
+    int q=1;
+    while (q<10){//Si quieres que tambien imprima el 10 pones <=
+        cout << q << endl;
+        q=q+1; }
+    q=9;
+    while (q>=1){
+        cout << q << endl;
+        q=q-1;
         }
 
-//Multiplos de 3 menores a 100
+/*Multiplos de 3 menores a 100
     int x=1;
     while (x<=100){
         if (x%3==0){
             cout << x << " " << endl;}
         x=x+1;//O tambien puedes poner x++;
-        }
-//Solicita un numero por teclado e indica si es primo                    NO ESTA BIEN, MUY COMPLEJO
-    int num, y=1, w=0;
+        }                                             */
+
+//Solicita un numero por teclado e indica si es primo
+    long long num, u = 1 , div=0; //Puso el long long para numeros mas grandes
     cout << "Indica un numero: " << endl;
     cin >> num;
-    while (y<=num){
-            if (num%y==0){
-                w++;}
+    while (u<=num){ //Usa i como variable auxiliar
+        if (num%u==0){ //Un primo es divisible entre el mismo y el 1, si su residuo entre esos 2 numeros es 0 es primo
+            div++;  //Cada vez que sea divisible (o sea por 2 numeros) se le suma, porque todo primo tiene 2 divisores
+            }
+        u++;//Aca incrementa el valor de j para seguir con el while y dividir del 1 hasta el numero que puso
     }
-    cout << ( (w == 2) ? "Primo" : "No primo") << endl;
+    cout << ( (div == 2) ? "Primo" : "No primo") << endl; //Aca verifica si el Num tiene 2 o mas divisores, si solo tiene 2 es primo
 
 //Pide un programa que pida un numero e imprima los numeros primos menores que este
-    int a, d=1,z=0;
+    long long n, j=2,d=0, i=2;
     cout << "Dame un numero: " << endl;
-    cin >> a;
-    while (d<=a){
-            if (a%d==0){
-                z++;
-
+    cin >> n;
+    while (j<=n){
+        if (j==2){
+            cout << "2 ";
+            j++;
+            continue;
+        }
+        while (i*i <=j){
+            if (j%i==0){
+                d++;
+                break;
             }
+            i++;
+        }
+        if (d==0){
+            cout << j << " ";}
+        d=0; i=2;
+        j++;
+    }
+//Escribe un programa que pida un numero (3 a 9 digitos) y evalua si el numero es un palindromo
+    int a, a_invertida;
+    cout << "\nDame un numero de 3 a 9 digitos\n";
+    cin >> a;
+    while (0<a){
+        int digito =a%10;
+        a_invertida=a_invertida + digito;
+        a=a/10; }
+    if (a == a_invertida){
+        cout << a << "Es palindromo\n"; }
+    else {
+        cout << a << "No es palindromo\n";                     //PORQUE ME SALE UN 0 ACA, ADEMAS HACERLO ACA CON FOR Y WL PRIMO TAMBIEN
     }
 
 /*Bucle FOR
-    for (inicia una variable (si ya esta iniciada lo dejas en blanco) ; condicion ; incrementacion)
+    for (inicia una variable {si ya esta iniciada lo dejas en blanco} ; condicion ; incrementacion)
         instruccion 1
         instruccion 2 */
 
